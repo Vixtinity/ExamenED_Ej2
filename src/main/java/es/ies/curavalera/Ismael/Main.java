5689fec5dbd86f9c6b5935f8ceff0524a100cf11
Vixtinity <08062006ismael@gmail.com>
2025-03-20 09:41:49 +0100

    segundo commit

--- src/main/java/es/ies/curavalera/Ismael/Main.java
+++ src/main/java/es/ies/curavalera/Ismael/Main.java
@@ -19,7 +19,7 @@ public class Main {
         empleado1.setDireccion("C/del Pez,6");
         empleado1.setNumHijos(3);
         System.out.println("DNI: "+ empleado1.getDni());
-        System.out.println("Nombre: "+ empleado1.getNombre());
+        System.out.println("Nombr: "+ empleado1.getNombre());
         System.out.println("Cargo: "+empleado1.getCargo());
         System.out.println("Número de hijos: "+ empleado1.getNumHijos());
         System.out.println("Sueldo: "+empleado1.calcula_sueldo(1200));
